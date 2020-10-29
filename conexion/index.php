642136de9f8e4e3c2de1e5f302b4d9791cce08ad
sayda2002 <huayllanihuamansayda@gmail.com>
2020-10-29 17:45:59 -0500

    subiendo archivos

--- conexion/index.php
+++ conexion/index.php
@@ -1,10 +1,11 @@
 <?php 
 $con = new mysqli("localhost","root","","sayda");
 
+/*
 if ($con) {
 	echo "bien conectado";
 }else{
 	echo "mal conectado";
-}	
+}	*/
 
  ?>
--- index.php
+++ index.php
@@ -1,1 +1,141 @@
-index.php
+<?php 
+include 'conexion/index.php';
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php 
+	/*
+	$var1 = 12;
+	echo $var1;
+
+	$var2 = 13.15;
+	echo $var2;
+
+	$var3 = "hola";
+	echo $var3;
+
+	$var4 = $var1.$var3;
+	echo $var4;
+
+	$var5 = $var4;
+	echo $var5;
+*/
+	/*if (1==2) {
+		echo "uno";
+	}else{
+		echo "dos";
+
+	}
+	$var = 1;
+	switch ($var) {
+		case '1':
+			# code...
+			echo "uno";
+			break;
+		case '2':
+			# code...
+			echo "dos";
+			break;
+		case '3':
+			# code...
+			echo "tres";
+			break;
+		case '4':
+			# code...
+			echo "cuatro";
+			break;
+		case '5':
+			# code...
+			echo "cinco";
+			break;
+		
+		default:
+			# code...
+			echo "hola";
+			break;
+	}*/
+	$var = "agosto";
+	switch ($var) {
+		case 'enero':
+			# code...
+			echo "cumplaños de mi papa";
+			break;
+		case 'febrero':
+			# code...
+			echo "San Valentin";
+			break;
+		case 'marzo':
+			# code...
+			echo "cumpleaños de Frank";
+			break;
+		case 'abril':
+			# code...
+			echo "cumpleaños de Moises";
+			break;
+		case 'mayo':
+			# code...
+			echo "Dia de la Madre";
+			break;
+		case 'junio':
+			# code...
+			echo "cumplaños de Sayda";
+			break;
+		case 'julio':
+			# code...
+			echo "cumplaños de Roberto";
+			break;
+		case 'agosto':
+			# code...
+			echo "cumpleaños de mi mama <br></br>";
+			echo "cumpleaños de Eliane";
+			break;
+		case 'setiembre':
+			# code...
+			echo "cumpleaños de Grober";
+			break;
+		case 'octubre':
+			# code...
+			echo "cumpleaños de Ana";
+			break;
+		case 'noviembre':
+			# code...
+			echo "cumpleaños de Gedeon Mario";
+			break;
+		case 'diciembre':
+			# code...
+			echo "cumpleaños de Juan";
+			break;
+		default:
+			# code...
+			echo "fuiste chama";
+			break;	}
+	/*
+	for ($i=0; $i < ; $i++) { 
+		# code...
+	}
+
+	foreach ($variable as $key => $value) {
+		# code...
+	}
+	while ( <= 10) {
+		# code...
+	}
+*/
+
+
+
+
+
+	 ?>
+
+
+	
+</body>
+</html>
